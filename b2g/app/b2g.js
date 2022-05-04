d28f772531369508f071e50e7b911bcb5c020a30
Fabrice Desré <fabrice@desre.org>
2022-05-04 12:15:51 -0700

    Bug 131383 - Update b2g prefs to allow https scripts loading from the system app. r=me

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -1285,9 +1285,10 @@ pref("security.disallow_privileged_https_subdocuments_loads", false);
 // Workaround https://bugzilla.mozilla.org/show_bug.cgi?id=1735117 since
 // the system app needs to load resources from http://shared.localhost
 pref("security.disallow_privileged_https_stylesheet_loads", false);
+pref("security.disallow_privileged_https_script_loads", false);
 
 #ifdef MOZ_WIDGET_GONK
 pref("b2g.api-daemon.uds-socket", "/dev/socket/api-daemon");
 #else
-pref("b2g.api-daemon.uds-socket", "/tmp/api-daemon-socket")
+pref("b2g.api-daemon.uds-socket", "/tmp/api-daemon-socket");
 #endif

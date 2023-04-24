ca224c815621a42a1905d4a93ee4a0dd99a67fca
Fabrice Desré <fabrice@desre.org>
2023-04-24 02:14:48 -0700

    B2G changes for MozBug 1828413

--- servo/components/style/gecko/media_features.rs
+++ servo/components/style/gecko/media_features.rs
@@ -717,7 +717,7 @@ macro_rules! bool_pref_feature {
 /// to support new types in these entries and (2) ensuring that either
 /// nsPresContext::MediaFeatureValuesChanged is called when the value that
 /// would be returned by the evaluator function could change.
-pub static MEDIA_FEATURES: [QueryFeatureDescription; 68] = [
+pub static MEDIA_FEATURES: [QueryFeatureDescription; 69] = [
     feature!(
         atom!("width"),
         AllowsRanges::Yes,

052fefa4afbee239e7c6b94f52137aacdbee874e
Ethan Chen <ethan.chen@kaiostech.com>
2024-02-22 15:06:47 +0800

    Bug 138059 - Fix build errors when ACCESSIBILITY is enabled. r=shelly.lin

--- config/make-system-wrappers.py
+++ config/make-system-wrappers.py
@@ -14,6 +14,17 @@ header_template = """#pragma GCC system_header
 include_next_template = "#include_next <{header}>"
 
 
+def undef_macros(header, *macro_list):
+    push_str = ""
+    pop_str = ""
+    undef_str = ""
+    for macro in macro_list:
+        push_str += '#pragma push_macro("' + macro + '")\n'
+        undef_str += "#undef " + macro + "\n"
+        pop_str += '\n#pragma pop_macro("' + macro + '")'
+    return push_str + undef_str + header + pop_str
+
+
 # The 'unused' arg is the output file from the file_generate action. We actually
 # generate all the files in header_list
 def gen_wrappers(unused, outdir, *header_list):
@@ -35,4 +46,10 @@ def gen_wrappers(unused, outdir, *header_list):
                 # system header, so that our wayland-util.h wrapper is picked
                 # first.
                 includes = '#include "wayland-util.h"\n' + includes
+            elif header.split("/")[-1] == "AudioSystem.h":
+                # Enabling ACCESSIBILITY in Gecko causes name conflicts with
+                # an enum in AudioStreamType.h on AOSP 13. To avoid compilation
+                # errors, we undefine ACCESSIBILITY in the wrapper of
+                # AudioSystem.h, which indirectly includes AudioStreamType.h.
+                includes = undef_macros(includes, "ACCESSIBILITY")
             f.write(header_template.format(includes=includes))

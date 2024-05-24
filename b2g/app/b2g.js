682267c9cbcbb344383bfec06c1e727bbaddfb8e
Ethan Chen <ethan.chen@kaiostech.com>
2024-05-24 20:55:08 +0800

    Bug 138863 - Allow creation of NativeFontResourceFreeType. r=shelly.lin

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -1256,9 +1256,6 @@ pref("font.name-list.emoji", "KaiOS Emoji");
 // Disable the path check in file system.
 pref("dom.filesystem.pathcheck.disabled", true);
 
-// Disable the native font source of freetype.
-pref("gfx.font_rendering.native_font_source_none", true);
-
 // KaiOS Captive Portal URL
 pref("captivedetect.canonicalURL", "http://detectportal.kaiostech.com/success.txt");
 pref("captivedetect.canonicalContent", "success");
